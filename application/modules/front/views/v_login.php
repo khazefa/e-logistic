a3647a42ef0704254fefea14dab6b2f24ab799c5
Khazefa IV <cybergitt@gmail.com>
2018-06-12 17:02:04 +0700

    Initial Local Commit

---
+++ application/modules/front/views/v_login.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>E-Logistic | Login</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta content="<?php APP_NAME;?>" name="description" />
+        <meta content="cybergitt@gmail.com" name="author" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="<?php echo base_url();?>assets/public/images/favicon.png">
+
+        <!-- App css -->
+        <link href="<?php echo base_url();?>assets/public/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo base_url();?>assets/public/css/icons.css" rel="stylesheet" type="text/css" />
+        <link href="<?php echo base_url();?>assets/public/css/style.css" rel="stylesheet" type="text/css" />
+
+        <script src="<?php echo base_url();?>assets/public/js/modernizr.min.js"></script>
+
+    </head>
+    <!-- Begin page -->
+    <div class="container">
+    <br>
+    <h3 class="text-center"><?php echo APP_NAME;?> Diebold Nixdorf Indonesia</h3>
+    <hr>
+        <div class="row">
+        <aside class="col-md-4 offset-md-4">
+            <div class="card">
+            <article class="card-body">
+                <h4 class="card-title text-center mb-4 mt-1">Sign in</h4>
+                <hr>
+                <p class="text-success text-center">
+                    <?php
+                    $error = $this->session->flashdata('error');
+                    if($error)
+                    {
+                    ?>
+                    <div class="alert alert-danger alert-dismissable" role="alert">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                            <?php echo $error; ?>                    
+                    </div>
+                    <?php
+                    }
+                    $success = $this->session->flashdata('success');
+                    if($success)
+                    {
+                    ?>
+                    <div class="alert alert-success alert-dismissable">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                            <?php echo $success; ?>                    
+                    </div>
+                    <?php } ?>
+                </p>
+                <form id="sign_in" action="<?php echo base_url('login/proccess'); ?>" method="POST" class="form-validate" ecntype="application/x-www-form-urlencoded">
+                    <input type="hidden" name="<?php echo $this->security->get_csrf_token_name();?>" value="<?php echo $this->security->get_csrf_hash();?>">
+                    <div class="form-group">
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+                             </div>
+                            <input type="text" class="form-control" name="username" placeholder="Username" required autofocus>
+                        </div> <!-- input-group.// -->
+                    </div> <!-- form-group// -->
+                    <div class="form-group">
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+                             </div>
+                            <input type="password" class="form-control" name="password" placeholder="******" required>
+                        </div> <!-- input-group.// -->
+                    </div> <!-- form-group// -->
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary btn-block"> Login  </button>
+                    </div> <!-- form-group// -->
+                    <p class="text-center"><a href="#" class="btn">Forgot password?</a></p>
+                </form>
+            </article>
+            </div> <!-- card.// -->
+        </aside>
+        </div>
+    </div>
+    <!-- End page -->
+    
+    <!-- jQuery  -->
+    <script src="<?php echo base_url();?>assets/public/js/jquery.min.js"></script>
+    <script src="<?php echo base_url();?>assets/public/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url();?>assets/public/js/jquery.slimscroll.js"></script>
+
+    <!-- App js -->
+    <script src="<?php echo base_url();?>assets/public/js/jquery.core.js"></script>
+    <script src="<?php echo base_url();?>assets/public/js/jquery.app.js"></script>
+
+    </body>
+</html>

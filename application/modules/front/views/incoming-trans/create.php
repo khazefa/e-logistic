e9c8d998a2054ce1d2714cc68d3e3551245fe436
khazefion <cybergitt@gmail.com>
2018-07-10 20:28:23 +0700

    Change Incoming Transaction Interface

--- application/modules/front/views/incoming-trans/create.php
+++ application/modules/front/views/incoming-trans/create.php
@@ -4,12 +4,11 @@
         <div class="card-box">
             <h4 class="header-title m-b-20"><?php echo $contentTitle;?></h4><hr>
             <div class="card-body">
-                
                 <div class="row">
-                    <div class="col col-md-6">
-                        <div class="form-group row">
-                            <label for="fpurpose" class="col-3 col-form-label">Purpose</label>
-                            <div class="col-6">
+                    <div class="form-group form-group-sm col-sm-6">
+                        <div class="row">
+                            <label for="fpurpose" class="col-sm-3 col-form-label">Purpose</label>
+                            <div class="col-sm-6">
                                 <select name="fpurpose" id="fpurpose" class="form-control" placeholder="Select Purpose">
                                     <option value="0">Select Purpose</option>
                                     <option value="S">Supply</option>
@@ -17,51 +16,60 @@
                                 </select>
                             </div>
                         </div>
-                        <div class="form-group row">
-                            <label for="fticketnum" class="col-3 col-form-label">Ticket Number</label>
-                            <div class="col-6">
-                                <input type="text" name="fticketnum" id="fticketnum" class="form-control" required="required" pattern="\s*">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="form-group form-group-sm col-sm-6">
+                        <div class="row">
+                            <label for="fticketnum" class="col-sm-3 col-form-label">Ticket Number</label>
+                            <div class="col-sm-6">
+                                <input type="text" name="fticketnum" id="fticketnum" class="form-control" required="required" pattern="[a-zA-Z0-9\s]+">
                             </div>
                         </div>
-                        <div class="form-group row">
-                            <label for="fengineer_id" class="col-3 col-form-label">FE ID Number</label>
-                            <div class="col-6">
+                    </div>
+                    <div class="form-group form-group-sm col-sm-6">
+                        <div class="row">
+                            <label for="fengineer_id" class="col-sm-3 col-form-label">FE ID Number</label>
+                            <div class="col-sm-6">
                                 <input type="text" name="fengineer_id" id="fengineer_id" class="form-control" required="required">
                                 <span id="feg_notes" class="help-block"><small></small></span>
                             </div>
                         </div>
-                        <div class="form-group row">
-                            <label for="fnotes" class="col-3 col-form-label">Delivery Notes</label>
-                            <div class="col-6">
+                    </div>
+                    <div class="form-group form-group-sm col-sm-6">
+                        <div class="row">
+                            <label for="fnotes" class="col-sm-3 col-form-label">Delivery Notes</label>
+                            <div class="col-sm-6">
                                 <input type="text" name="fnotes" id="fnotes" class="form-control">
                             </div>
                         </div>
                     </div>
-                    <div class="col col-md-6">
-                        <div class="form-group row">
-                            <label for="fpartner" class="col-3 col-form-label">Service Partner</label>
-                            <div class="col-6">
+                    <div class="form-group form-group-sm col-sm-6">
+                        <div class="row">
+                            <label for="fpartner" class="col-sm-3 col-form-label">Service Partner</label>
+                            <div class="col-sm-6">
                                 <input type="text" name="fpartner" id="fpartner" class="form-control" readonly="readonly">
                             </div>
                         </div>
-                        <div class="form-group row">
-                            <label for="fengineer_name" class="col-3 col-form-label">Assigned FE</label>
-                            <div class="col-6">
+                    </div>
+                    <div class="form-group form-group-sm col-sm-6">
+                        <div class="row">
+                            <label for="fpurpose_notes" id="fpurpose_notes" class="col-sm-9 col-form-label"></label>                            
+                        </div>
+                    </div>
+                    <div class="form-group form-group-sm col-sm-6">
+                        <div class="row">
+                            <label for="fengineer_name" class="col-sm-3 col-form-label">Assigned FE</label>
+                            <div class="col-sm-6">
                                 <input type="text" name="fengineer_name" id="fengineer_name" class="form-control" readonly="readonly">
                             </div>
                         </div>
-                        <div class="form-group row">
-                            <label for="freturn_notes" id="freturn_notes" class="col-9 col-form-label"></label>
-                        </div>
                     </div>
                 </div>
-                </form>
             </div>
         </div>
     </div>
-</div>
     
-<div class="row">
     <div class="col-md-9">
         <div class="card-box">
             <div class="card-header bg-primary text-white">
@@ -70,33 +78,36 @@
 
             <div class="card-body">
                 <div class="row">
+                    <div class="form-group form-group-sm col-sm-6">
+                        <div class="row">
+                            <div class="input-group col-sm-12">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"> <i class="fa fa-barcode"></i> </span>
+                                 </div>
+                                <input type="text" name="fpartnum" id="fpartnum" class="form-control" placeholder="Part Number" required="required">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group form-group-sm col-sm-6">
+                        <div class="row">
+                            <div class="input-group col-sm-12">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"> <i class="fa fa-barcode"></i> </span>
+                                 </div>
+                                <input type="text" name="fserialnum" id="fserialnum" class="form-control" placeholder="Serial Number">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group form-group-sm col-sm-12">
+                        <hr>
+                    </div>
+                </div>
+                <div class="row">
                     <div class="col col-md-12">
-                        <div class="row form-group">
-                            <div class="col col-md-6">
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"> <i class="fa fa-barcode"></i> </span>
-                                     </div>
-                                    <input type="text" name="fpartnum" id="fpartnum" class="form-control" placeholder="Part Number" required="required">
-                                </div>
-                            </div>
-                            <div class="col col-md-6">
-                                <div class="input-group">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"> <i class="fa fa-barcode"></i> </span>
-                                     </div>
-                                    <input type="text" name="fserialnum" id="fserialnum" class="form-control" placeholder="Serial Number">
-                                </div>
-                                <!--<span class="help-block" id="fpartname">Part Name</span>-->
-                            </div>
-                        </div>
-                        <div class="row form-group">
-                            <div class="col col-md-12"><hr></div>
-                        </div>
 
                         <div class="row">
                             <div class="col col-md-12">
-                            <table id="cart_grid" class="table table-bordered table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
+                            <table id="cart_grid" class="table table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
                                 <thead>
                                 <tr>
                                     <th>X</th>
@@ -113,11 +124,10 @@
                             </div>
                         </div>
 
-                        <div class="mt-4"></div>
+                        <div class="mt-2"></div>
 
                         <div class="row">
-                            <div class="col col-md-10">&nbsp;</div>
-                            <div class="col col-md-2">
+                            <div class="col-md-2 offset-md-10">
                                 Total Quantity: <span id="ttl_qty">10</span>
                             </div>
                         </div>
@@ -125,17 +135,12 @@
                 </div>
             </div>
             <div class="card-footer">
-                <div class="row">
-                    <div class="col col-md-4">
-                        <button type="button" class="btn btn-block btn-danger">Cancel</button>
-                    </div>
-                    <div class="col col-md-4">
-                        <button type="button" class="btn btn-block btn-warning">Hold</button>
-                    </div>
-                    <div class="col col-md-4">
-                        <button type="submit" class="btn btn-block btn-success">Request</button>
-                    </div>
+                <div class="button-list">
+                    <button type="button" class="btn btn-block btn-danger waves-effect waves-light">Cancel</button>
+                    <button type="button" class="btn btn-block btn-warning waves-effect waves-light">Hold</button>
+                    <button type="submit" class="btn btn-block btn-success waves-effect waves-light">Request</button>
                 </div>
+
             </div>
         </div>
     </div>
@@ -160,7 +165,7 @@
     var e_notes = $('#fnotes');
     var e_partner = $('#fpartner');
     var e_engineer_name = $('#fengineer_name');
-    var e_return_notes = $('#freturn_notes');
+    var e_purpose_notes = $('#fpurpose_notes');
     
     var e_partnum = $('#fpartnum');
     var e_serialnum = $('#fserialnum');
@@ -180,7 +185,7 @@
         e_engineer_notes.html("");
         e_notes.prop("readonly", true);
         e_notes.prop("value", "");
-        e_return_notes.html("");
+        e_purpose_notes.html("Purpose Notes");
     }
     
     function get_eg_detail(eg_id){
@@ -319,13 +324,14 @@
                 e_engineer_id.prop("readonly", true);
                 e_engineer_id.prop("value", "");
                 e_notes.prop("readonly", false);
-                e_return_notes.html("");
+                e_purpose_notes.html("Purpose Notes");
             }else if(valpurpose === "RG"){
                 e_ticketnum.prop("readonly", false);
+                e_ticketnum.focus();
                 e_engineer_id.prop("readonly", false);
                 e_notes.prop("readonly", true);
                 e_notes.prop("value", "");
-                e_return_notes.html("Pengembalian Barang ke FSL");
+                e_purpose_notes.html("Pengembalian Barang ke FSL");
             }else{
                 alert( "Please choose purpose!" );
                 init_form();

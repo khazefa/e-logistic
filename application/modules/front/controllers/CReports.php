ad32f013cdf10d13707e039bfb51879a86826624
khazefion <cybergitt@gmail.com>
2018-09-09 15:11:48 +0700

    Add FE Report column to used part report

--- application/modules/front/controllers/CReports.php
+++ application/modules/front/controllers/CReports.php
@@ -850,8 +850,8 @@ class CReports extends BaseController
                 $spreadsheet->setActiveSheetIndex($x);
                 $activeSheet = $spreadsheet->getActiveSheet();
                 
-                $activeSheet->mergeCells('A1:J1');
-                $activeSheet->mergeCells('A2:J2');
+                $activeSheet->mergeCells('A1:K1');
+                $activeSheet->mergeCells('A2:K2');
                 $activeSheet
                 ->setCellValue('A1', $fslname)
                 ->setCellValue('A2', 'Report Date: '.$reportdateID.' to '.$reportdateID2);
@@ -865,12 +865,13 @@ class CReports extends BaseController
                 ->setCellValue('D4', 'Outgoing No.')
                 ->setCellValue('E4', 'Ticket No.')
                 ->setCellValue('F4', 'Purpose')
-                ->setCellValue('G4', 'Status')
-                ->setCellValue('H4', 'FSE ID')
-                ->setCellValue('I4', 'Assigned FSE')
-                ->setCellValue('J4', 'Partner')
+                ->setCellValue('G4', 'FE Report')
+                ->setCellValue('H4', 'Status')
+                ->setCellValue('I4', 'FSE ID')
+                ->setCellValue('J4', 'Assigned FSE')
+                ->setCellValue('K4', 'Partner')
                 ;
-                $activeSheet->getStyle('A4:J4')->applyFromArray($styleHeaderArray);
+                $activeSheet->getStyle('A4:K4')->applyFromArray($styleHeaderArray);
 
                 //Parameters for cURL
                 $arrWhere = array('fcode'=> strtoupper($fcode), 'fdate1'=> $fdate1.' 00:00:00', 'fdate2'=> $fdate2.' 23:59:59');
@@ -887,6 +888,7 @@ class CReports extends BaseController
                     $transnum = filter_var($row->outgoing_num, FILTER_SANITIZE_STRING);
                     $ticket = filter_var($row->outgoing_ticket, FILTER_SANITIZE_STRING);
                     $purpose = filter_var($row->outgoing_purpose, FILTER_SANITIZE_STRING);
+                    $fereport = filter_var($row->fe_report, FILTER_SANITIZE_STRING);
                     $status = filter_var($row->status, FILTER_SANITIZE_STRING);
                     $engineer = filter_var($row->engineer_key, FILTER_SANITIZE_STRING);
                     $engineer_name = filter_var($row->engineer_name, FILTER_SANITIZE_STRING);
@@ -903,11 +905,14 @@ class CReports extends BaseController
                     $activeSheet->getColumnDimension('H')->setAutoSize(true);
                     $activeSheet->getColumnDimension('I')->setAutoSize(true);
                     $activeSheet->getColumnDimension('J')->setAutoSize(true);
+                    $activeSheet->getColumnDimension('K')->setAutoSize(true);
                     $activeSheet->getStyle('C')->getNumberFormat()->setFormatCode(\PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_TEXT);
                     $activeSheet->getStyle('E')->getNumberFormat()->setFormatCode(\PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_TEXT);
                     $activeSheet->getStyle('H')->getNumberFormat()->setFormatCode(\PhpOffice\PhpSpreadsheet\Style\NumberFormat::FORMAT_TEXT);
                     $activeSheet->getStyle('F')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+                    $activeSheet->getStyle('G')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
                     $activeSheet->getStyle('H')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
+                    $activeSheet->getStyle('I')->getAlignment()->setHorizontal(\PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER);
 
                     //fill data row
                     $activeSheet
@@ -917,10 +922,11 @@ class CReports extends BaseController
                     ->setCellValue('D'.$i, $transnum)
                     ->setCellValue('E'.$i, $ticket)
                     ->setCellValue('F'.$i, $purpose)
-                    ->setCellValue('G'.$i, strtoupper($status))
-                    ->setCellValue('H'.$i, $engineer)
-                    ->setCellValue('I'.$i, $engineer_name)
-                    ->setCellValue('J'.$i, $partner_name);
+                    ->setCellValue('G'.$i, $fereport)
+                    ->setCellValue('H'.$i, strtoupper($status))
+                    ->setCellValue('I'.$i, $engineer)
+                    ->setCellValue('J'.$i, $engineer_name)
+                    ->setCellValue('K'.$i, $partner_name);
 
                     $i++;
                 }

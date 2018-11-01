1da980578ab619fd1a18659a5cb8d0844186d449
khazefion <cybergitt@gmail.com>
2018-11-01 16:34:50 +0700

    Update application/libraries/Enums.php
    Add new function

--- application/libraries/Enums.php
+++ application/libraries/Enums.php
@@ -2,69 +2,96 @@
 
 class Enums
 {
-	public function enumsUserLevel($intEnc){
-		// $CI =& get_instance();
-		
-		// if($CI->Roles_models->get_data($intEnc){
-		// 	return "Anonymous";
-		// }else{
-		// 	return $arrLevel[$intEnc];
-		// }
+    public function enumsUserLevel($intEnc){
+        // $CI =& get_instance();
 
-		$arrLevel = array(ROLE_ABOVE => "One Above All", ROLE_SU => "Super User", ROLE_USER => "User");
-		
-		if ($intEnc == 0){
-                    return "Anonymous";
-		}else{
-                    return $arrLevel[$intEnc];
-		}
-	}
+        // if($CI->Roles_models->get_data($intEnc){
+        // 	return "Anonymous";
+        // }else{
+        // 	return $arrLevel[$intEnc];
+        // }
 
-	public function enums_department($ID){
-//            $CI =& get_instance();
-//            $CI->db->select('dept_name, dept_alias');
-//            $CI->db->where('dept_id = "'.$ID.'"');
-//            $query=$CI->db->get('client_departments');
-//            $query=$query->row_array();
-//            return $query['userlvl'];
-            
-            $arrLevel = array(1 => "Claim", 2 => "Finance", 3 => "IT", 4 => "GA");
+        $arrLevel = array(ROLE_ABOVE => "One Above All", ROLE_SU => "Super User", ROLE_USER => "User");
 
-            if ($intEnc == 0){
-                return "Non-Department";
-            }else{
-                return $arrLevel[$intEnc];		
-            }
-	}
+        if ($intEnc == 0){
+            return "Anonymous";
+        }else{
+            return $arrLevel[$intEnc];
+        }
+    }
 
-	public function enumsGender($intId){
-		$arrGender = array(0 => "Male", 1 => "Female", 2 => "unknown");
-		
-		// if ((int)$intId != 0 || (int)$intId != 1 || (int)$intId != 2 ){
-		// 	return $arrGender;
-		// }else{
-			return $arrGender[$intId];		
-		// }
-	}
-	
-	public function enumsMonthName($intId){
-		$arrMonth = array(1 => "Januari", 
-                                    2 => "Februari", 
-                                    3 => "Maret", 
-                                    4 => "April", 
-                                    5 => "Mei", 
-                                    6 => "Juni", 
-                                    7 => "Juli", 
-                                    8 => "Agustus", 
-                                    9 => "September", 
-                                    10 => "Oktober", 
-                                    11 => "November", 
-                                    12 => "Desember");
-		if ($intId == 0){
-                    return $arrMonth;		
-		}else{
-                    return $arrMonth[$intId];		
-		}
-	}
+    public function enums_department($ID){
+//        $CI =& get_instance();
+//        $CI->db->select('dept_name, dept_alias');
+//        $CI->db->where('dept_id = "'.$ID.'"');
+//        $query=$CI->db->get('client_departments');
+//        $query=$query->row_array();
+//        return $query['userlvl'];
+
+        $arrLevel = array(1 => "Claim", 2 => "Finance", 3 => "IT", 4 => "GA");
+
+        if ($intEnc == 0){
+            return "Non-Department";
+        }else{
+            return $arrLevel[$intEnc];		
+        }
+    }
+
+    public function enumsGender($intId){
+        $arrGender = array(0 => "Male", 1 => "Female", 2 => "unknown");
+
+        // if ((int)$intId != 0 || (int)$intId != 1 || (int)$intId != 2 ){
+        // 	return $arrGender;
+        // }else{
+                return $arrGender[$intId];		
+        // }
+    }
+
+    public function enumsMonthName($intId){
+        $arrMonth = array(1 => "Januari", 
+                            2 => "Februari", 
+                            3 => "Maret", 
+                            4 => "April", 
+                            5 => "Mei", 
+                            6 => "Juni", 
+                            7 => "Juli", 
+                            8 => "Agustus", 
+                            9 => "September", 
+                            10 => "Oktober", 
+                            11 => "November", 
+                            12 => "Desember");
+        if ($intId == 0){
+            return $arrMonth;		
+        }else{
+            return $arrMonth[$intId];		
+        }
+    }
+    
+    public function enumsPurpose($purpose_code){
+        switch ($purpose_code){
+            case "SP";
+                $purpose = "Sales/Project";
+            break;
+            case "W";
+                $purpose = "Warranty";
+            break;
+            case "M";
+                $purpose = "Maintenance";
+            break;
+            case "I";
+                $purpose = "Investments";
+            break;
+            case "B";
+                $purpose = "Borrowing";
+            break;
+            case "RWH";
+                $purpose = "Transfer Stock";
+            break;
+            default;
+                $purpose = "-";
+            break;
+        }
+        return $purpose;
+    }
 }
 

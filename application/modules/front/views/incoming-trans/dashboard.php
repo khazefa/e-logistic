39ecad3b15ee5b05d53098c7a5d228c41dd03fb9
khazefion <cybergitt@gmail.com>
2018-11-06 18:42:19 +0700

    Update application/modules/front/views/incoming-trans/dashboard.php

---
+++ application/modules/front/views/incoming-trans/dashboard.php
@@ -0,0 +1,38 @@
+<div class="row text-center">
+    <div class="col-sm-6 col-lg-6 col-xl-4">
+        <div class="card-box widget-flat border-custom bg-custom text-white">
+            <i class="fi-archive"></i>
+            <h3 class="m-b-10">25563</h3>
+            <p class="text-uppercase m-b-5 font-13 font-600">Total Return Parts</p>
+            <div class="btn-group">
+                <button type="button" onclick="location.href='<?php echo base_url('return-parts/view');?>'" class="btn btn-warning waves-light waves-effect">
+                    View Detail
+                </button>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6 col-lg-6 col-xl-4">
+        <div class="card-box bg-primary widget-flat border-primary text-white">
+            <i class="fi-archive"></i>
+            <h3 class="m-b-10">6952</h3>
+            <p class="text-uppercase m-b-5 font-13 font-600">Total Supply from Another FSL</p>
+            <div class="btn-group">
+                <button type="button" onclick="location.href='<?php echo base_url($classname.'/add');?>'" class="btn btn-warning waves-light waves-effect">
+                    View Detail
+                </button>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6 col-lg-6 col-xl-4">
+        <div class="card-box widget-flat border-success bg-success text-white">
+            <i class="fi-archive"></i>
+            <h3 class="m-b-10">18361</h3>
+            <p class="text-uppercase m-b-5 font-13 font-600">Total Supply from Central Warehouse</p>
+            <div class="btn-group">
+                <button type="button" onclick="location.href='<?php echo base_url($classname.'/add');?>'" class="btn btn-warning waves-light waves-effect">
+                    View Detail
+                </button>
+            </div>
+        </div>
+    </div>
+</div>

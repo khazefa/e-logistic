2d415358383944a3cf86c5e1c9edd5828be1f8ba
khazefion <cybergitt@gmail.com>
2018-11-21 14:11:02 +0700

    Update dashboard.php

--- application/modules/front/views/outgoing-trans/dashboard.php
+++ application/modules/front/views/outgoing-trans/dashboard.php
@@ -1,5 +1,5 @@
 <div class="row text-center">
-    <div class="col-sm-6 col-lg-6 col-xl-6">
+    <div class="col-sm-4 col-lg-4 col-xl-4">
         <div class="card-box widget-flat border-custom bg-custom text-white">
             <i class="fi-archive"></i>
             <p class="text-uppercase m-b-5 font-18 font-600">Request Parts</p>
@@ -19,8 +19,8 @@
             </div>
         </div>
     </div>
-    <div class="col-sm-6 col-lg-6 col-xl-6">
-        <div class="card-box bg-primary widget-flat border-primary text-white">
+    <div class="col-sm-4 col-lg-4 col-xl-4">
+        <div class="card-box widget-flat border-custom bg-custom text-white">
             <i class="fi-archive"></i>
             <p class="text-uppercase m-b-5 font-18 font-600">Transfer Stock to FSL</p>
             <div class="button-list">
@@ -34,6 +34,26 @@
                     }
                 ?>
                 <button type="button" onclick="location.href='<?php echo base_url('transfer-stock-to-fsl/view');?>'" class="btn btn-warning waves-light waves-effect">
+                    View Detail
+                </button>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-4 col-lg-4 col-xl-4">
+        <div class="card-box widget-flat border-custom bg-custom text-white">
+            <i class="fi-archive"></i>
+            <p class="text-uppercase m-b-5 font-18 font-600">Transfer Bad Part/Bad Stock to Central Warehouse</p>
+            <div class="button-list">
+                <?php
+                    if(!$readonly){
+                ?>
+                <button type="button" onclick="location.href='<?php echo base_url('new-fsltocwh-trans');?>'" class="btn btn-default waves-light waves-effect">
+                    Add New
+                </button>
+                <?php
+                    }
+                ?>
+                <button type="button" onclick="location.href='<?php echo base_url('fsltocwh-trans');?>'" class="btn btn-warning waves-light waves-effect">
                     View Detail
                 </button>
             </div>
